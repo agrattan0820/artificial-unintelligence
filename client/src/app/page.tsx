35671eb7a69ef3bd1ec525115fbdb29fc0b33bb7
GameDog9988 <alex@grattan.me>
2023-04-28 19:39:02 -0400

    feat: add room page and order classes on homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,24 +1,24 @@
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center min-h-screen">
+    <main className="flex min-h-screen flex-col justify-center">
       <section className="container mx-auto px-4">
-        <h1 className="text-6xl mb-8">beeeeeeeep</h1>
+        <h1 className="mb-8 text-6xl">beeeeeeeep</h1>
         <div className="relative mb-8">
           <input
             id="nickname"
-            className="peer h-10 bg-transparent border-b-2 px-2 border-l-2 border-gray-400 placeholder-transparent focus:border-indigo-600 focus:outline-none"
+            className="peer h-10 border-b-2 border-l-2 border-gray-400 bg-transparent px-2 placeholder-transparent focus:border-indigo-600 focus:outline-none"
             type="text"
             placeholder="enter a nickname"
           />
           <label
             htmlFor="nickname"
-            className="absolute transition-all left-2 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+            className="absolute -top-3.5 left-2 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
           >
             Enter a cool nickname
           </label>
         </div>
         <div className="space-x-2">
-          <button className="bg-indigo-600 text-white px-4 transition hover:bg-indigo-500 focus:bg-indigo-700">
+          <button className="bg-indigo-600 px-4 text-white transition hover:bg-indigo-500 focus:bg-indigo-700">
             Start Game
           </button>
           <button className="bg-gray-300 px-4 transition hover:bg-gray-200 focus:bg-gray-400">
---
+++ client/src/app/room/[code]/page.tsx
@@ -0,0 +1,7 @@
+export default function Room() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center">
+      <h1 className="text-6xl">beeeeeeeep room</h1>
+    </main>
+  );
+}
