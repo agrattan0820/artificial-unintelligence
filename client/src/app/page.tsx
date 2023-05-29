b6f9917dc7b3bd6a1c0823f82ace418d77ebb80a
GameDog9988 <alex@grattan.me>
2023-05-29 15:10:06 -0400

    refactor: remove test queries from homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,11 @@
 import NicknameForm, { NicknameFormType } from "@ai/components/nickname-form";
-import supabase from "@ai/utils/supabase";
-import { getRoomInfo } from "./actions";
 
 export default async function Home() {
-  const { data } = await supabase.from("users").select();
-  const roomInfo = await getRoomInfo("0b947326");
-
-  console.log(data);
-  console.log(roomInfo);
-
   return (
     <main className="flex min-h-screen flex-col justify-center">
       <section className="container mx-auto px-4">
         <h1 className="mb-8 text-6xl">beeeeeeeep</h1>
         <NicknameForm submitLabel="Start Game" type="HOME" />
-        <pre>{JSON.stringify(data, null, 2)}</pre>
       </section>
     </main>
   );
