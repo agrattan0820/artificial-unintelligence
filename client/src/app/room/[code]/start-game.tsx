ebebe93c3877beff29bab5bd226dd3c64d806b3d
GameDog9988 <alex@grattan.me>
2023-07-03 08:00:42 -0400

    feat: edit start-game component to require at least 3 players

--- client/src/app/room/[code]/start-game.tsx
+++ client/src/app/room/[code]/start-game.tsx
@@ -1,14 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { FiCheckSquare, FiPlusSquare, FiPlay } from "react-icons/fi";
+
 import { User } from "@ai/app/server-actions";
 import Button, { SecondaryButton } from "@ai/components/button";
 import Ellipsis from "@ai/components/ellipsis";
-import useIsMounted from "@ai/utils/hooks/use-is-mounted";
 import useShare from "@ai/utils/hooks/use-share";
-import { useStore } from "@ai/utils/store";
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import { FiCheckSquare, FiPlusSquare, FiPlay } from "react-icons/fi";
 
 const StartGame = ({
   players,
@@ -36,7 +35,7 @@ const StartGame = ({
 
   return (
     <div className="mt-8 flex items-center justify-center gap-2">
-      {players.length > 1 && (
+      {players.length > 2 && (
         <Button onClick={onStartGame} className="flex items-center gap-2">
           {loading ? (
             <Ellipsis />
