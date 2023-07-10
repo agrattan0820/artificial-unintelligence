9631a4629a778b6bd9e6f5fb01f270e89e42c33d
GameDog9988 <alex@grattan.me>
2023-07-10 07:50:41 -0400

    refactor: eslint disable animation controls useEffect

--- client/src/components/game/winner.tsx
+++ client/src/components/game/winner.tsx
@@ -48,6 +48,7 @@ const Winner = ({ gameInfo, state, send }: WinnerProps) => {
     const controls = animate(count, 1500, { duration: 3 });
 
     return controls.stop;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
