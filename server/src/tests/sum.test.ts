0051b04059ef6ee0945d8b018765ff05f8d1015d
GameDog9988 <alex@grattan.me>
2023-07-18 16:48:16 -0400

    feat: support testing with `jest`

---
+++ server/src/tests/sum.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "@jest/globals";
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
