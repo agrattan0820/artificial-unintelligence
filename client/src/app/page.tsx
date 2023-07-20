2aec876f5bd0251b9346f314d0a440415266b88d
GameDog9988 <alex@grattan.me>
2023-07-20 08:27:40 -0400

    feat: initial name: `aritificial unintelligence`

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-center">
       <section className="container mx-auto px-4">
-        <h1 className="mb-8 text-6xl">beeeeeeeep</h1>
+        <h1 className="mb-8 text-6xl">
+          artif<span className="">i</span>cial <br /> unintelligence
+        </h1>
         <NicknameForm submitLabel="Start Game" type="HOME" />
       </section>
     </main>
