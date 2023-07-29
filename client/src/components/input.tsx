07c06fa5c605eeeb6f3cc4a2e46a9241321b821e
GameDog9988 <alex@grattan.me>
2023-07-29 10:23:59 -0400

    fix: ensure input is not rounded for iOS mobile

--- client/src/components/input.tsx
+++ client/src/components/input.tsx
@@ -22,14 +22,14 @@ const Input = ({
         {...props}
         id={id}
         className={cn(
-          `peer h-10 border-b-2 border-l-2 border-gray-400 bg-transparent px-2 placeholder-transparent focus:border-indigo-600 focus:outline-none`,
+          `peer h-10 rounded-none border-b-2 border-l-2 border-gray-400 bg-transparent px-2 placeholder-transparent focus:border-indigo-600 focus:outline-none`,
           className
         )}
       />
       <label
         htmlFor={id}
         className={cn(
-          `absolute -top-3.5 left-2 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600 dark:text-gray-400 dark:peer-focus:text-gray-400`,
+          `absolute -top-3.5 left-2 rounded-none text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600 dark:text-gray-400 dark:peer-focus:text-gray-400`,
           labelStyles
         )}
       >
