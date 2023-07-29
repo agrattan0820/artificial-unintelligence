8d929c270f75e1fec271c8b2218ff2a9c741bb38
GameDog9988 <alex@grattan.me>
2023-07-29 10:20:47 -0400

    feat: add friend to homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,18 @@
+import Friend from "@ai/components/game/friend";
+import FriendWithLegs from "@ai/components/game/friend-with-legs";
 import NicknameForm from "@ai/components/nickname-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center">
-      <section className="container mx-auto px-4">
-        <h1 className="mb-8 text-6xl">
-          artif<span className="">i</span>cial <br /> unintelligence
-        </h1>
-        <NicknameForm submitLabel="Start Game" type="HOME" />
+    <main className="pt-36 md:flex md:min-h-screen md:flex-col md:justify-center md:pt-0">
+      <section className="container mx-auto flex flex-col-reverse items-start justify-center gap-8 px-4 lg:flex-row lg:gap-24">
+        <div>
+          <h1 className="mb-8 text-4xl md:text-6xl">
+            artif<span className="">i</span>cial <br /> unintelligence
+          </h1>
+          <NicknameForm submitLabel="Start Game" type="HOME" />
+        </div>
+        <Friend className="w-32 lg:w-1/4" />
       </section>
     </main>
   );
