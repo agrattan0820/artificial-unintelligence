389583acb5ac1072baf04c70a439eee329bb674d
GameDog9988 <alex@grattan.me>
2023-07-29 11:57:50 -0400

    feat: make connection screens responsive

--- client/src/components/game/connect-to-mainframe.tsx
+++ client/src/components/game/connect-to-mainframe.tsx
@@ -1,15 +1,14 @@
-"use client";
-
 import Image from "next/image";
 import TypewriterText from "@ai/components/typewriter";
 
 const ConnectToMainframe = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <h2 className="mb-6 w-fit text-4xl">
+      <h2 className="mb-6 w-fit text-lg md:text-4xl">
         <TypewriterText>Connecting to the mainframe...</TypewriterText>
       </h2>
       <Image
+        className="px-4"
         src="https://media.giphy.com/media/B4dt6rXq6nABilHTYM/giphy.gif"
         alt="Hacker man"
         width={498}
--- client/src/components/game/connection-established.tsx
+++ client/src/components/game/connection-established.tsx
@@ -3,7 +3,7 @@ import TypewriterText from "@ai/components/typewriter";
 const ConnectionEstablished = () => {
   return (
     <div className="mx-auto flex min-h-screen w-fit flex-col items-center justify-center">
-      <h2 className="text-4xl">
+      <h2 className="text-lg md:text-4xl">
         <TypewriterText>Beginning training sequence...</TypewriterText>
       </h2>
     </div>
