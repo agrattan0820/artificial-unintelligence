51f2cc5b4ebdc4a6b37ef62a8b9b11993c8d2568
GameDog9988 <alex@grattan.me>
2023-08-07 08:42:26 -0400

    feat: lobby screen improvements

--- client/src/app/room/[code]/invite-link.tsx
+++ client/src/app/room/[code]/invite-link.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 import { FiCheckSquare, FiCopy } from "react-icons/fi";
 
 import useShare from "@ai/utils/hooks/use-share";
 
 const InviteLink = ({ code }: { code: string }) => {
-  const { link, copying, setCopying, onClick } = useShare(`/invite/${code}`);
+  const { link, copying, setCopying, onClick } = useShare(
+    `/invite/${code}`,
+    () => toast("Invite Link Copied to Clipboard")
+  );
 
   useEffect(() => {
     if (copying) {
@@ -19,13 +23,12 @@ const InviteLink = ({ code }: { code: string }) => {
 
   return (
     <button
-      className="mx-auto flex w-full items-center justify-center gap-2 rounded-xl border-2 border-indigo-600 p-8 text-sm underline-offset-2 hover:underline sm:w-auto md:text-xl"
+      className="mx-auto flex w-full items-center justify-center gap-2 rounded-xl border-2 border-indigo-600 p-8 text-xl underline-offset-2 hover:underline sm:w-auto"
       onClick={onClick}
     >
-      {link}
-      <span className="hidden md:inline-block">
-        {copying ? <FiCheckSquare /> : <FiCopy />}
-      </span>
+      <span className="sm:hidden">{code}</span>
+      <span className="hidden sm:inline-block">{link}</span>
+      <span>{copying ? <FiCheckSquare /> : <FiCopy />}</span>
     </button>
   );
 };
