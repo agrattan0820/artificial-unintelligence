870e5b89c61a26e49015c42efdfd610194c6fa13
GameDog9988 <alex@grattan.me>
2023-08-12 11:47:37 -0400

    feat: separate playwright tests into separate workflow

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
- testDir: "src/e2e",
-   use: {
-    baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
+  testDir: "src/e2e",
+  use: {
+    baseURL: process.env.CI
+      ? "https://artificial-unintelligence-git-dev-agrattan.vercel.app"
+      : "http://localhost:3000",
   },
-   webServer: {
-    command: 'pnpm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
-})
+  webServer: !process.env.CI
+    ? [
+        {
+          command: "pnpm run dev",
+          url: "http://localhost:3000",
+        },
+        {
+          command: "cd .. && cd server && pnpm run dev",
+          url: "http://localhost:8080",
+        },
+      ]
+    : undefined,
+});
