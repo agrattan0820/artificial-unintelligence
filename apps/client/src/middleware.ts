1ce38afe98f025c5bd4becac334f45968c0cbcb4
Alexander Grattan <alex@grattan.me>
2023-10-03 16:44:06 -0400

    feat: use upstash redis instead of vercel kv

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
 
 const ratelimit = new Ratelimit({
-  redis: kv,
+  redis: Redis.fromEnv(),
   // 5 requests from the same IP in 10 seconds
   limiter: Ratelimit.slidingWindow(5, "10 s"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
 });
 
 export const config = {
