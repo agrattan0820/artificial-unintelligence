ebc8e56862775f802bb06b9fbeec780190c07af3
GameDog9988 <alex@grattan.me>
2023-10-03 13:25:12 -0400

    fix: remove tls option

--- apps/server/src/redis.ts
+++ apps/server/src/redis.ts
@@ -1,12 +1,7 @@
 import Redis from "ioredis";
 
 const redis = new Redis(
-  process.env.REDIS_PRIVATE_URL ?? process.env.REDIS_URL ?? "",
-  {
-    tls: {
-      rejectUnauthorized: false,
-    },
-  }
+  process.env.REDIS_PRIVATE_URL ?? process.env.REDIS_URL ?? ""
 );
 
 // Listen to connection events to prevent TCP Issue
